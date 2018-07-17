d1f0a6ee3210c61a4cb0245f4db7630bb2154d7e
Angel <angel@twdt260.ict888.net>
2018-07-17 15:42:04 +0800

    story book basic environment

---
+++ my-app/src/stories/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+import APP from '../App';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('This is a test', () => (
+    <Button onClick={action('clicked')}>
+    Test</Button>
+    ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+storiesOf("App", module)
+  .add("default",() => <APP title="React"/>)
+  .add("with title",() => <APP title="My World" />);
